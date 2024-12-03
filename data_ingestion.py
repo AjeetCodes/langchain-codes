b09b7bda92a67866e63f7e27308042ae02669d06
Ajeet Yadav <ajeet.yadav@pay1.in>
2024-12-03 09:03:57 +0530

    added sentence transformer for embedings

--- data_ingestion.py
+++ data_ingestion.py
@@ -4,6 +4,7 @@ from langchain_community.document_loaders import TextLoader
 
 load_dotenv()
 
+# os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 # text documents loader
 loader = TextLoader("speech.txt")
 text_documents = loader.load()
@@ -25,5 +26,36 @@ loader = WebBaseLoader(
 )
 
 text_documents = loader.load()
-print(text_documents)
-# os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+# print(text_documents)
+
+## Pdf reader
+from langchain_community.document_loaders import PyPDFLoader
+loader=PyPDFLoader('attention.pdf')
+docs=loader.load()
+
+# print(docs[0].page_content)
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+documents=text_splitter.split_documents(docs)
+# print(documents[:5])
+# raise SystemExit("exit")
+## Vector Embedding And Vector Store
+from sentence_transformers import SentenceTransformer
+from langchain_community.vectorstores import Chroma
+
+embedingModel = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Function to generate embeddings
+class CustomEmbeddings:
+    def embed_documents(self, texts):
+        # return embedding_model.encode(texts, convert_to_numpy=True)
+        return embedingModel.encode(texts, convert_to_numpy=True).tolist()
+    def embed_query(self, query):
+        """Generate an embedding for a single query."""
+        return embedingModel.encode(query, convert_to_numpy=True).tolist()
+    
+db = Chroma.from_documents(documents, CustomEmbeddings())
+query = "Who are the authors of attention is all you need?"
+retireved_results=db.similarity_search(query)
+print(retireved_results[0].page_content)
